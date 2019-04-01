6d7282972a61ab9a133428416f88342727eb3a0e
burhanuddin <dinburhanuddin26@gmail.com>
2019-04-01 16:56:46 +0700

    update credential

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "tcp:burhanuddinwebappserver.database.windows.net,1433";
+    $user = "burhanuddin";
+    $pass = "Dic0ding@2019";
+    $db = "burhanuddindb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
